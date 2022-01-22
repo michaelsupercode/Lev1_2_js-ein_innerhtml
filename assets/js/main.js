c00c7577019138c5b10472c9ee335184e05812f2
SuperCoder <supercoder@AirvonSperCoder.fritz.box>
2022-01-22 15:14:28 +0100

    inner

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+let info = document.getElementById("info").innerHTML = "..hello world!!";
+document.write("<h2>How are you today?</h2>");
+
+let container = document.getElementById('container').innerHTML = '<p>start of that element</p>';
+document.write('end of this element');
